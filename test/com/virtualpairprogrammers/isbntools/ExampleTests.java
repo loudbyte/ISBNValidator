c60e56096e383803b5365ff6da1162a7a2339580
Dave <davebottomhell@gmail.com>
2021-10-17 04:26:06 +0600

    TDD ExampleTests.checkAnInvalidISBN RED PART

--- test/com/virtualpairprogrammers/isbntools/ExampleTests.java
+++ test/com/virtualpairprogrammers/isbntools/ExampleTests.java
@@ -2,14 +2,22 @@ package com.virtualpairprogrammers.isbntools;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ExampleTests {
 
     @Test
-    public void exampleTest() {
+    public void checkAValidIDBN() {
         ValidateISBN validateISBN = new ValidateISBN();
         boolean result = validateISBN.checkISBN(1140449116);
         assertTrue(result);
+    }
+
+    @Test
+    public void checkAnInvalidISBN() {
+        ValidateISBN validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN(1140449117);
+        assertFalse(result);
     }
 }
